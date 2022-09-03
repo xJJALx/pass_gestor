195990f81c3d2c2129a22e98e3278fa1bf3ee2cf
Jhon <jhonjairookawaii@gmail.com>
2022-09-03 18:13:37 +0200

    Fix lower case name

---
+++ lib/data/datasources/data_tests.dart
@@ -0,0 +1,13 @@
+import 'package:pass_gestor/data/models/models.dart';
+
+List<Password> itemPassDataSource = [
+  Password(listpassId: 1, name: 'name', password: 'password'),
+  Password(listpassId: 2, name: 'test1', password: 'pass1'),
+  Password(listpassId: 3, name: 'test2', password: 'pass2'),
+];
+
+List<Group> groupNameDataSource = [
+  Group(id: 1, uid: 'uid1', name: 'name'),
+  Group(id: 2, uid: 'uid2', name: 'list1'),
+  Group(id: 3, uid: 'uid3', name: 'list2'),
+];
